6b5d5035a20b00c92393f4810079b701e33ee3a8
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-03-30 15:45:15 -0300

    Dia 1
    Aprendi sobre prints, sintaxis de java, uso de clases, compilacion

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Perro wuf = new Perro();
+        String saludo = wuf.saludar();
+        System.out.println(saludo);
+
+    }
+}
